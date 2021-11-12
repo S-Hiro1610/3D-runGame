b6a19b84739cc8343c9ad19b82f38736f8873de1
S-Hiro <hiroto-_-1610@docomo.ne.jp>
2021-11-12 17:49:48 +0900

    プレイヤーの外へ出ないようにする処理を追加

--- Assets/Scripts/PlayerGuard.cs
+++ Assets/Scripts/PlayerGuard.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class PlayerGuard : MonoBehaviour
 {
-    Transform _transform;
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            Debug.Log("PlayerIN");
-            other.transform.position = new Vector3(other.transform.position.x, 0, 0);
+    [SerializeField] GameObject _player = default;
+    [SerializeField] GameObject _stage = default;
+    float _stageSizeX;
+    float _playerSizeX;
 
+    private void Start()
+    {
+        _stageSizeX = _stage.GetComponent<Renderer>().bounds.size.x;
+        _playerSizeX = _player.GetComponent<Renderer>().bounds.size.x;
+    }
+    private void Update()
+    {
+        if (_player.transform.position.x > _stageSizeX / 2 - _playerSizeX / 2)
+        {
+            _player.transform.position = new Vector3(_stageSizeX / 2 - _playerSizeX / 2, transform.position.y, 0);
+        }
+        if (_player.transform.position.x < -_stageSizeX / 2 + _playerSizeX / 2)
+        {
+            _player.transform.position = new Vector3(-_stageSizeX / 2 + _playerSizeX / 2, transform.position.y, 0);
         }
     }
 }
